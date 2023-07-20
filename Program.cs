b468d12d323fa027e23acce8a4c8bc156bdf9f03
SiriusCrack <MorganBrockman1998@Gmail.com>
2023-07-19 19:51:40 -0700

    completion

--- Program.cs
+++ Program.cs
@@ -1,2 +1,120 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace RetailLockboxAssessment {
+    class Program {
+        private static HashSet<int> intNums = new HashSet<int>();
+        private static HashSet<float> floatNums = new HashSet<float>();
+        private static StreamReader FileReader = StreamReader.Null;
+        private static string FileDate = "XXXXXX";
+        private static string FilePath = "C:\\retaillockbox\\XXXXXXImport.txt";
+
+        public static void Main(string[] args) {
+            FileDate = ParseDate(args);
+            OpenFile();
+            ReadNumbers();
+            WriteCSV();
+        }
+
+        private static string ParseDate(string[] args) {
+            string inputDate = args[0];
+            try {
+                if(args.Length != 1) { throw new ArgumentException("invalid arg count"); }
+                if(
+                    inputDate.Length != 8 ||
+                    !int.TryParse(inputDate, out _)
+                ) { throw new ArgumentException("\"" + inputDate + "\" date invalid"); }
+            } catch(ArgumentException ex) {
+                Console.WriteLine("Exception: " + ex.Message);
+                Console.WriteLine("Please provide the file date");
+                Console.WriteLine("Usage: Program <YYYYMMDD>");
+                Environment.Exit(1);
+            }
+                StringBuilder outputDateBuilder = new StringBuilder();
+                outputDateBuilder.Append(inputDate[4]);
+                outputDateBuilder.Append(inputDate[5]);
+                outputDateBuilder.Append(inputDate[6]);
+                outputDateBuilder.Append(inputDate[7]);
+                outputDateBuilder.Append(inputDate[2]);
+                outputDateBuilder.Append(inputDate[3]);
+                string outputDate = outputDateBuilder.ToString();
+                return outputDate;
+        }
+
+        private static void OpenFile() {
+            FilePath = "C:\\retaillockbox\\" + FileDate + "Import.txt";
+            // FilePath = "./testInput.txt";
+            try {
+                if(!File.Exists(FilePath)) { throw new FileNotFoundException("File not found: " + FilePath); }
+            } catch(FileNotFoundException ex) {
+                Console.WriteLine("Exception: " + ex.Message);
+                Environment.Exit(1);
+            }
+            FileReader = new StreamReader(FilePath);
+        }
+
+        private static void ReadNumbers() {
+            bool emptyFile = true;
+            try {
+                while(!FileReader.EndOfStream) {
+                    string? line = FileReader.ReadLine();
+                    if(line != null) {
+                        emptyFile = false;
+                        char[] splitChars = {' ', '\t', '%', '*', '+', ',', '-', '/', ':', '<', '=', '>'};
+                        string[] words = line.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+                        foreach(string word in words) {
+                            string cleanedWord = RemoveSymbols(word);
+                            int intNum;
+                            bool isInt = false;
+                            isInt = int.TryParse(cleanedWord, out intNum);
+                            if(isInt) { intNums.Add(intNum); }
+                            else {
+                                float floatNum;
+                                bool isFloat = false;
+                                isFloat = float.TryParse(cleanedWord, out floatNum);
+                                if(isFloat) { floatNums.Add(floatNum); }
+                            }
+                        }
+                    }
+                }
+                if(emptyFile) { throw new InvalidDataException("Empty file: " + FilePath); }
+            } catch(InvalidDataException ex) {
+                Console.WriteLine("Exception: " + ex.Message);
+                Environment.Exit(1);
+            }
+        }
+
+        private static string RemoveSymbols(string word) {
+            string? newWord = null;
+            int i = 0;
+            foreach(char c in word) {
+                if(char.IsSymbol(c)) {
+                    newWord = word.Remove(i, 1);
+                }
+                i++;
+            }
+            if(newWord != null) { return newWord; }
+            else { return word; }
+        }
+
+        private static void WriteCSV() {
+            string csvPath = "C:\\retaillockbox\\" + FileDate + "Export.csv";
+            // string csvPath = "./testOutput.csv";
+            try {
+                if(File.Exists(csvPath)) { throw new FileNotFoundException("File already exists: " + csvPath); }
+            } catch(FileNotFoundException ex) {
+                Console.WriteLine("Exception: " + ex.Message);
+                Console.Write("Overwrite? (y/N): ");
+                string? consoleInput = Console.ReadLine();
+                if(!string.Equals(consoleInput, "Y", StringComparison.OrdinalIgnoreCase)) { Environment.Exit(0); }
+            }
+            string intString = string.Join(',', intNums);
+            string floatString = string.Join(',', floatNums);
+            string numString = intString + "," + floatString;
+            StreamWriter csvWriter = new StreamWriter(csvPath);
+            csvWriter.WriteLine(numString);
+            csvWriter.Flush();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# RetailLockboxAssessment
+# Retail Lockbox, Inc. Assessment
+
+### Applicant Name
+Morgan Brockman
+
+### Program Assumptions
+* Program assumes no numbers separated by commas. (Ex: 300,200 read as 300 and 200, rather than 300200).
+* Program assumes no numbers exceed size allowed by int and float datatypes.
+
+### Time Estimate
+Estimated just under an hour to complete.  
+If I had more time I would probably do some more testing. I haven't really thoroughly tested all the exception cases.
